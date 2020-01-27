14ea90a8f698b61a6db5a75da4200635d07766bb
Sidharth Das <sidharth2189@gmail.com>
2020-01-28 01:58:35 +0530

    constructors in C++

---
+++ class/constructor_in_class.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cassert>
+using std::cout;
+
+class Date {
+ public:
+  Date(int d, int m, int y) {  // This is a constructor.
+    Day(d);
+    // Month(m);
+    // Year(y);
+  }
+  int Day() { return day; }
+  void Day(int d) {
+    if (d >= 1 && d <= 31) day = d;
+  }
+  int Month() { return month; }
+  void Month(int m) {
+    if (m >= 1 && m <= 12) month = m;
+  }
+  int Year() { return year; }
+  void Year(int y) { year = y; }
+
+ private:
+  int day{1};
+  int month{1};
+  int year{0};
+};
+
+// Test in main
+int main() {
+    Date date(8,29,1981);
+    cout << date.Day() << '\n';
+    cout << date.Month() << '\n';
+    cout << date.Year() << '\n';
+    assert(date.Day() == 8);
+    assert(date.Month() == 29);
+    assert(date.Year() == 1981);
+}
