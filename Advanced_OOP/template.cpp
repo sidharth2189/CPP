9d30056b46ce9ce045290fa81eab97b49916a9c2
Sidharth Das <sidharth2189@gmail.com>
2020-02-02 17:52:46 +0530

    Updated template

--- Advanced_OOP/template.cpp
+++ Advanced_OOP/template.cpp
@@ -12,15 +12,20 @@
     At compile time, the compiler then expands the code using the types that are passed as parameters.
 */
 # include <iostream>
-#include <assert.h>
+# include <assert.h>
+
+// Create a generic, templatized function Max()
+template <typename Type> Type Max(Type a, Type b) { return a > b ? a : b; }
 
 // Create a generic function Sum that adds two parameters
 template <typename Type> Type Sum(Type a, Type b) { return a + b; }
 
 // Create a generic function Product that multiplies two parameters
-template <typename Type> Type Product (Type a, Type b) {return a*b;}
+template <typename Type> Type Product (Type a, Type b) {return a*b; }
 
 int main() { 
     std::cout << Sum<double>(20.0, 13.7) << "\n";
     assert(Product<int>(10, 2) == 20);
+    assert(Max(10, 50) == 50);
+    assert(Max(5.7, 1.436246) == 5.7);
 }
