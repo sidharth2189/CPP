01ba4ed4bb1be34133f484bbad0d5fd7a99a0894
Sidharth Das <sidharth2189@gmail.com>
2020-02-05 22:34:59 +0530

    Accessing address

--- Memory_Management/debugger_gdb/debugger_GDB.cpp
+++ Memory_Management/debugger_gdb/debugger_GDB.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <iostream>
+using std::cout;
 
 int main()
 {
     char str1[]= "UDACITY";
-    printf("%s",str1);
+    printf("%s\n",str1);
+    cout << &str1 << '\n';
 
     return 0;
 }
