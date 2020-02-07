bb6d0d33a7c4e4fa3ed0837502ebff75f7da354e
Sidharth Das <sidharth2189@gmail.com>
2020-02-08 02:38:39 +0530

    Malloc function vs new operator

---
+++ Memory_Management/heap/malloc_vs_new.cpp
@@ -0,0 +1,52 @@
+/*
+    1-Constructors/Destructors:
+    Unlike malloc( sizeof(MyClass) ), the call new MyClass() calls the constructor. 
+    Similarly, delete calls the destructor.
+
+    2-Type safety: malloc returns a void pointer, which needs to be cast into the 
+    appropriate data type it points to. This is not type safe, as one can freely 
+    vary the pointer type without any warnings or errors from the compiler
+*/
+
+#include <stdlib.h>
+#include <iostream>
+
+class MyClass
+{
+private:
+    int *_number;
+
+public:
+    MyClass()
+    {
+        std::cout << "Allocate memory\n";
+        _number = (int *)malloc(sizeof(int));
+    }
+    ~MyClass()
+    {
+        std::cout << "Delete memory\n";
+        free(_number);
+    }
+    void setNumber(int number)
+    {
+        *_number = number;
+        std::cout << "Number: " << _number << "\n";
+    }
+};
+
+
+int main()
+{
+    // allocate memory using malloc
+    // comment these lines out to run the example below
+    // MyClass *myClass = (MyClass *)malloc(sizeof(MyClass));
+    // myClass->setNumber(42); // EXC_BAD_ACCESS
+    // free(myClass);
+	  
+	// allocate memory using new
+    MyClass *myClass = new MyClass();
+    myClass->setNumber(42); // works as expected
+    delete myClass;
+
+    return 0;
+}
