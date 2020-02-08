515a67a5e6779b0e67bd33e56fd9f9a281473f95
Sidharth Das <sidharth2189@gmail.com>
2020-02-08 18:21:49 +0530

    Shared ownership policy

---
+++ Memory_Management/heap/Resource_copying_policies/shared_ownership.cpp
@@ -0,0 +1,63 @@
+/*
+    The idea is to perform a copy or assignment similar to the default behavior, 
+    i.e. copying the handle instead of the content (as with a shallow copy) while 
+    at the same time keeping track of the number of instances that also point to 
+    the same resource. Each time an instance goes out of scope, the counter is 
+    decremented. Once the last object is about to be deleted, it can safely 
+    deallocate the memory resource.
+*/
+
+#include <iostream>
+
+class SharedCopy
+{
+private:
+    int *_myInt;
+    static int _cnt;
+
+public:
+    SharedCopy(int val);
+    ~SharedCopy();
+    SharedCopy(SharedCopy &source);
+};
+
+int SharedCopy::_cnt = 0;
+
+SharedCopy::SharedCopy(int val)
+{
+    _myInt = (int *)malloc(sizeof(int));
+    *_myInt = val;
+    ++_cnt;
+    std::cout << "resource allocated at address " << _myInt << std::endl;
+}
+
+SharedCopy::~SharedCopy()
+{
+    --_cnt;
+    if (_cnt == 0)
+    {
+        free(_myInt);
+        std::cout << "resource freed at address " << _myInt << std::endl;
+    }
+    else
+    {
+        std::cout << "instance at address " << this << " goes out of scope with _cnt = " << _cnt << std::endl;
+    }
+}
+
+SharedCopy::SharedCopy(SharedCopy &source)
+{
+    _myInt = source._myInt;
+    ++_cnt;
+    std::cout << _cnt << " instances with handles to address " << _myInt << " with _myInt = " << *_myInt << std::endl;
+}
+
+int main()
+{
+    SharedCopy source(42);
+    SharedCopy destination1(source);
+    SharedCopy destination2(source);
+    SharedCopy destination3(source);
+
+    return 0;
+}
