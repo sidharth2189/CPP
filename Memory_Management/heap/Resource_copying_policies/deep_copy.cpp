3ede6166aa65ad9b17e7201a8856203392ecb623
Sidharth Das <sidharth2189@gmail.com>
2020-02-08 18:01:28 +0530

    deep copy

---
+++ Memory_Management/heap/Resource_copying_policies/deep_copy.cpp
@@ -0,0 +1,52 @@
+/*
+    The deep-copy version of MyClass looks similar to the exclusive ownership policy: 
+    Both the assignment operator and the copy constructor have been overloaded with the 
+    source object passed by reference. But instead of copying the source handle (and then 
+    deleting it), a proprietary block of memory is allocated on the heap and the content 
+    of the source is copied into it.
+
+    All copies have the same value while the address of the handle differs.
+*/
+
+#include <iostream>
+
+class DeepCopy
+{
+private:
+    int *_myInt;
+
+public:
+    DeepCopy(int val)
+    {
+        _myInt = (int *)malloc(sizeof(int));
+        *_myInt = val;
+        std::cout << "resource allocated at address " << _myInt << std::endl;
+    }
+    ~DeepCopy()
+    {
+        free(_myInt);
+        std::cout << "resource freed at address " << _myInt << std::endl;
+    }
+    DeepCopy(DeepCopy &source)
+    {
+        _myInt = (int *)malloc(sizeof(int));
+        *_myInt = *source._myInt;
+        std::cout << "resource allocated at address " << _myInt << " with _myInt = " << *_myInt << std::endl;
+    }
+    DeepCopy &operator=(DeepCopy &source)
+    {
+        _myInt = (int *)malloc(sizeof(int));
+        std::cout << "resource allocated at address " << _myInt << " with _myInt=" << *_myInt << std::endl;
+        *_myInt = *source._myInt;
+        return *this;
+    }
+};
+
+int main()
+{
+    DeepCopy source(42);
+    DeepCopy dest1(source);
+    DeepCopy dest2 = dest1;
+
+    return 0;
+}
