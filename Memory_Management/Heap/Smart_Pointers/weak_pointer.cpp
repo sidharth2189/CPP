82dd67d25e85c177b4266b408dfda5a2ec0e4bbb
Sidharth Das <sidharth2189@gmail.com>
2020-02-10 02:55:09 +0530

    Weak pointer

---
+++ Memory_Management/Heap/Smart_Pointers/weak_pointer.cpp
@@ -0,0 +1,31 @@
+/*
+    Similar to shared pointers, there can be multiple weak pointers to the same resource. 
+    The main difference though is that weak pointers do not increase the reference count. 
+    Weak pointers hold a non-owning reference to an object that is managed by another shared pointer.
+
+    The following rule applies to weak pointers: You can only create weak pointers out of shared 
+    pointers or out of another weak pointer. 
+
+    First, a shared pointer to an integer is created with a reference count of 1 after creation. 
+    Then, two weak pointers to the integer resource are created, the first directly from the 
+    shared pointer and the second indirectly from the first weak pointer. As can be seen from 
+    the output, neither of both weak pointers increased the reference count. At the end of main, 
+    the attempt to directly create a weak pointer to an integer resource would lead to a compile error
+*/
+
+#include <iostream>
+#include <memory>
+
+int main()
+{
+    std::shared_ptr<int> mySharedPtr(new int);
+    std::cout << "shared pointer count = " << mySharedPtr.use_count() << std::endl;
+
+    std::weak_ptr<int> myWeakPtr1(mySharedPtr);
+    std::weak_ptr<int> myWeakPtr2(myWeakPtr1);
+    std::cout << "shared pointer count = " << mySharedPtr.use_count() << std::endl;
+
+    // std::weak_ptr<int> myWeakPtr3(new int); // COMPILE ERROR
+
+    return 0;
+}
