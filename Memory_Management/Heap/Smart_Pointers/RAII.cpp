da358bc53ca23fe6f43072eb34944b9d9d078837
Sidharth Das <sidharth2189@gmail.com>
2020-02-09 22:45:33 +0530

    RAII Example

--- Memory_Management/Heap/Smart_Pointers/RAII.cpp
+++ Memory_Management/Heap/Smart_Pointers/RAII.cpp
@@ -1,3 +1,11 @@
+/*
+    The constructor of class MyInt takes a pointer to a memory resource. 
+    When the destructor of a MyInt object is called, the resource is deleted 
+    from memory - which makes MyInt an RAII memory management class. Also, 
+    the * operator is overloaded which enables us to dereference MyInt objects 
+    in the same manner as with raw pointers
+*/
+
 # include <iostream>
 
 class MyInt
@@ -18,6 +26,22 @@ int main()
     double den[] = {1.0, 2.0, 3.0, 4.0, 5.0};
     for (size_t i = 0; i < 5; ++i)
     {
+        // allocate the resource on the stack
+        MyInt en(new int(i));
+
+        // use the resource
+        std::cout << *en << "/" << den[i] << " = " << *en / den[i] << std::endl;
+    }
+
+    return 0;
+}
+
+/*
+int main()
+{
+    double den[] = {1.0, 2.0, 3.0, 4.0, 5.0};
+    for (size_t i = 0; i < 5; ++i)
+    {
         // allocate the resource on the heap
         double *en = new double(i);
 
@@ -30,3 +54,4 @@ int main()
 
     return 0;
 }
+*/
