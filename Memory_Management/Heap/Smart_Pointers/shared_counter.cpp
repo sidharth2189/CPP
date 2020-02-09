25402e338efd005c03121e886f40e971f88c67f7
Sidharth Das <sidharth2189@gmail.com>
2020-02-10 01:46:20 +0530

    Update shared_counter.cpp

--- Memory_Management/Heap/Smart_Pointers/shared_counter.cpp
+++ Memory_Management/Heap/Smart_Pointers/shared_counter.cpp
@@ -1,3 +1,15 @@
+/*
+    We can see that shared pointers are constructed just as unique pointers are. 
+    Also, we can access the internal reference count by using the method use_count(). 
+    In the inner block, a second shared pointer shared2 is created and shared1 is 
+    assigned to it. In the copy constructor, the internal resource pointer is copied 
+    to shared2 and the resource counter is incremented in both shared1 and shared2
+
+    The lifetime of shared2 is limited to the scope denoted by the enclosing curly brackets. 
+    Thus, once this scope is left and shared2 is destroyed, the reference counter in shared1 
+    is decremented by one.
+*/
+
 #include <iostream>
 #include <memory>
 
