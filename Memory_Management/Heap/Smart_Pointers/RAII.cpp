027565360a5b1f56dd4c42768df6a66005285b22
Sidharth Das <sidharth2189@gmail.com>
2020-02-09 22:53:54 +0530

    Update RAII.cpp

--- Memory_Management/Heap/Smart_Pointers/RAII.cpp
+++ Memory_Management/Heap/Smart_Pointers/RAII.cpp
@@ -37,7 +37,22 @@ int main()
 }
 
 /*
-int main()
+    The part new int(i) creates a new block of memory on the heap and initializes it with the value of i. 
+    The returned result is the address of the block of memory.
+    The part MyInt en(…)calls the constructor of class MyInt, passing the address of a valid memory block 
+    as a parameter.
+    After creating an object of class MyInt on the stack, which, internally, created an integer on the heap, 
+    we can use the dereference operator in the same manner as before to retrieve the value to which the 
+    internal raw pointer is pointing. Because the MyInt object en lives on the stack, it is automatically 
+    deallocated after each loop cycle - which automatically calls the destructor to release the heap memory.
+
+    Above is an RAII implementation to create a memory management class that spares us from thinking about 
+    calling delete. By creating the MyInt object on the stack, we ensure that the deallocation occurs as 
+    soon as the object goes out of scope. 
+*/
+
+/* 
+int main() // without RAII
 {
     double den[] = {1.0, 2.0, 3.0, 4.0, 5.0};
     for (size_t i = 0; i < 5; ++i)
