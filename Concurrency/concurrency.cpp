4c9749c5ed67b488b4811f27fc52fb6f3eeec5f5
Sidharth Das <sidharth2189@gmail.com>
2020-02-12 23:38:37 +0530

    Main thread

---
+++ Concurrency/concurrency.cpp
@@ -0,0 +1,26 @@
+/*
+    The concurrency support in C++ makes it possible for a program to execute multiple 
+    threads in parallel. Concurrency was first introduced into the standard with C++11. 
+    Since then, new concurrency features have been added with each new standard update, 
+    such as in C++14 and C++17.
+
+    The standardization of concurrency in C++ now makes it possible to develop cross-platform 
+    concurrent programs, which is as significant improvement that saves time and reduces error 
+    proneness. Concurrency in C++ is provided by the thread support library, which can be 
+    accessed by including the header.
+
+    A running program consists of at least one thread. When the main function is executed, we 
+    refer to it as the "main thread". Threads are uniquely identified by their thread ID, which 
+    can be particularly useful for debugging a program. The code below prints the thread 
+    identifier of the main thread and outputs it to the console:
+*/
+
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    std::cout << "Hello concurrent world from main! Thread id = " << std::this_thread::get_id() << std::endl;
+
+    return 0;
+}
