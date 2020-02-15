dbfadcbce8841780f0356f312ed70839b1e5ae8d
Sidharth Das <sidharth2189@gmail.com>
2020-02-15 18:58:53 +0530

    Threading via Lambda prevents interleaving

---
+++ Concurrency/Multiple_Threads/Lambda_prevents_thread_interleaving.cpp
@@ -0,0 +1,40 @@
+/*
+    Let's adjust the program code from Fork_Join_Parallelism.cpp and use a Lambda 
+    instead of the function printHello(). Also, let's pass the loop counter i 
+    into the Lambda to enforce an individual wait time for each thread. The idea 
+    is to prevent the interleaving of text on the command line which we saw in 
+    the previous example
+*/
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <vector>
+
+int main()
+{
+    // create threads
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < 10; ++i)
+    {
+        // create new thread from a Lambda
+        threads.emplace_back([i]() {
+
+            // wait for certain amount of time
+            std::this_thread::sleep_for(std::chrono::milliseconds(10 * i));
+
+            // perform work
+            std::cout << "Hello from Worker thread #" << i << std::endl;
+        });
+    }
+
+    // do something in main()
+    std::cout << "Hello from Main thread" << std::endl;
+
+    // call join on all thread objects using a range-based loop
+    for (auto &t : threads)
+        t.join();
+
+    return 0;
+}
