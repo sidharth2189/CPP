ea4a1d151257baaee30c2c66204bb346144388f0
Sidharth Das <sidharth2189@gmail.com>
2020-02-23 17:51:34 +0530

    Increment test

---
+++ Sample/sample1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+using std::cout;
+
+int main()
+{
+    //Put your code here
+    int i = 10;
+    int j = ++i;
+    cout << i << " " << j << endl;
+    
+    return 0;
+}
