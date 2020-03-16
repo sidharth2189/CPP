8172e2126a63a579e9f29b7742a257153eefe739
Sidharth Das <sidharth2189@gmail.com>
2020-03-16 20:02:18 +0530

    Updated comments

--- Sample/pieces.cpp
+++ Sample/pieces.cpp
@@ -1,3 +1,9 @@
+/*
+    Input: Two vectors for e.g. original {1, 2, 4, 3} and desired {1, 4, 3, 2}
+    Output: Integer that indicates the minimum number of pieces to be made of original 
+    vector to make it look like the desired one.
+*/
+
 #include <string>
 #include <iostream>
 #include <stdexcept>
@@ -6,17 +12,24 @@
 
 int minPieces(const std::vector<int>& original, const std::vector<int>& desired)
 {
-    //throw std::logic_error("Waiting to be implemented");
-    int piece, vect_size, index;
-    piece = original.size();
-    vect_size = piece;
+    int index; // index: position in original for a given elemnt in desired
+    int piece = original.size(); // piece: initialize no. of pieces of original vector
+    int vect_size = piece; // size of the vectors
+
+    // Loop through the desired vector loop
     for (int i = 0 ; i < vect_size ; ++i)
     {
+        // Find location in original for the ith element in desired
         auto loc = std::find(original.begin(), original.end(), desired[i]);
         index = std::distance(original.begin(), loc);
-        while (original[index + 1] == desired[i + 1])
+
+        // Check if elements ahead of current element in desired vector matches sequence in original
+        while (original[index + 1] == desired[i + 1] && i < vect_size && index < vect_size)
         {
+            // Reduce assessment of number of pieces
             piece--;
+
+            // increment indices
             i++;
             index++;
         }   
@@ -30,5 +43,6 @@ int main()
     std::vector<int> original = { 1, 4, 3, 2 };
     std::vector<int> desired = { 1, 2, 4, 3 };
     std::cout << minPieces(original, desired) << std::endl;
+    return 0;
 }
 #endif
