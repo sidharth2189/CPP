3a80967a0617b61f1df22fbb367b12e32c98bb96
sidharth2189 <sidharth2189@gmail.com>
2024-09-18 21:16:21 +0200

    Update folder struct

--- Sample/Path_Planning/Discrete_PP/model_the_problem.cpp
+++ Sample/Path_Planning/Discrete_PP/model_the_problem.cpp
@@ -8,11 +8,52 @@ using namespace std;
 /* TODO: Define a Map class
    Inside the map class, define the mapWidth, mapHeight and grid as a 2D vector
 */
+class Map
+{
+public:
+    // Grid dimension
+    int mapWidth = 6;
+    int mapHeight = 5;
+    
+    // Grid
+    vector<vector<int>> grid
+    {
+        {0, 1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 1, 1, 0},
+    };
+};
 
 /* TODO: Define a Planner class
    Inside the Planner class, define the start, goal, cost, movements, and movements_arrows
    Note: The goal should be defined it terms of the mapWidth and mapHeight
 */
+class Planner: Map
+{
+public:
+    // Start position
+    vector<int> start = {0, 0};
+    
+    // Goal position
+    vector<int> goal = {mapHeight-1, mapWidth-1};
+    
+    // Cost of movement
+    int cost = 1;
+    
+    // Movement direction
+    vector<vector<int>> movements
+    {
+        {-1, 0}, // Up
+        {0, -1}, // Left
+        {1, 0},  // Down
+        {0, 1}   // Right
+    };
+    
+    // Movement arrows
+    vector<char> movements_arrows = {'^', '<', 'v', '>'};
+};
 
 /* TODO: Define a print2DVector function which will print 2D vectors of any data type
    Example
@@ -30,6 +71,17 @@ using namespace std;
    c d
    Hint: You need to use templates
 */
+template <typename T> void print2DVector(vector<vector<T>> vect)
+{
+    for (int row = 0; row < vect.size(); row++)
+    {
+        for (int col = 0; col < vect[row].size(); col++)
+        {
+            cout << vect[row][col] << " ";
+        }
+        cout << "\n";
+    }
+}
 
 /*############ Don't modify the main function############*/
 int main()
