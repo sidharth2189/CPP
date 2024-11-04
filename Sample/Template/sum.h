74f3a69f3892e23c0f6164656bdf327bbda356c9
Sidharth Das <sidharth2189@gmail.com>
2024-11-04 12:36:16 +0100

    Create sum.h

---
+++ Sample/Template/sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+#include <vector>
+using std::vector;
+
+// Compute Sum method declaration.
+template<typename T> T ComputeSum(vector<T> v);
+template<typename T> T ComputeSum(T x, T y);
+
+#include "sum.cpp" // Let compiler see definition when template instantiated
+
+#endif // SUM_H
