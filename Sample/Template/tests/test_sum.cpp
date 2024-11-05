edb4bb5c3ceed1a22c60a8075fe1a2e539716424
Sidharth Das <sidharth2189@gmail.com>
2024-11-05 15:55:42 +0100

    Update test_sum.cpp

--- Sample/Template/tests/test_sum.cpp
+++ Sample/Template/tests/test_sum.cpp
@@ -1,7 +1,7 @@
 # include <gtest/gtest.h>
 # include "sum.h"
 
-TEST(TestSum, InputAsVector)
+TEST(TestSum, InputAsVectorInt)
 {
     
     // Initialize vector with elements
@@ -15,14 +15,41 @@ TEST(TestSum, InputAsVector)
     EXPECT_EQ(total, expected);
 }
 
-TEST(TestSum, InputAsTwoElements)
+TEST(TestSum, InputAsVectorFloat)
+{
+    
+    // Initialize vector with elements
+    vector<float> v{1.1, 2.2, 3.3, 5.5};
+    float expected = 12.1;
+
+    // Compute sum of elements in vector
+    auto total = ComputeSum(v);
+
+    // Expect
+    EXPECT_EQ(total, expected);
+}
+
+TEST(TestSum, InputAsTwoElementsInt)
+{
+    // Initialize elements
+    int x = 3, y = 5;
+    int expected = 8;
+
+    // Compute sum of elements in vector
+    auto total = ComputeSum(x, y);
+
+    // Expect
+    EXPECT_EQ(total, expected);
+}
+
+TEST(TestSum, InputAsTwoElementsFloat)
 {
     // Initialize elements
     float x = 3.2, y = 5.1;
     float expected = 8.3;
 
     // Compute sum of elements in vector
-    float total = ComputeSum(x, y);
+    auto total = ComputeSum(x, y);
 
     // Expect
     EXPECT_EQ(total, expected);
